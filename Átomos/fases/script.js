1cc9a2ad92fc298119af85dbeab345a18ecfec27
LHanji <lohana100739@gmail.com>
2019-10-10 21:50:06 -0300

    Fase Átomos

---
+++ Átomos/fases/script.js
@@ -0,0 +1,115 @@
+function startFase(){
+
+    var elements = []
+
+    /* Pick random number to be the current fase */
+    var number = Math.floor(Math.random() * 5)
+    console.log(number)
+
+    /* get number */
+    document.getElementById("faseNum").value = number
+
+    /* Put the elements of that number on an array */
+    if(number == 0 || number == 4){
+        for(i = 0; i < 2; i++){
+            var element = {
+                src: "elements/" + number + "/" + i + ".png",
+            }
+            elements.push(element)
+        }
+    } else if(number == 1){
+        for(i = 0; i < 3; i++){
+            var element = {
+                src: "elements/" + number + "/" + i + ".png",
+            }
+            elements.push(element)
+        }
+    } else if(number == 2){
+        for(i = 0; i < 5; i++){
+            var element = {
+                src: "elements/" + number + "/" + i + ".png",
+            }
+            elements.push(element)
+        }
+    } else if(number == 3){
+        for(i = 0; i < 4; i++){
+            var element = {
+                src: "elements/" + number + "/" + i + ".png",
+            }
+            elements.push(element)
+        }
+    }
+
+    elements = randomSort(elements)
+    console.log(elements)
+    showFase(elements)
+
+    function showFase(elements){
+        var DOM_div = document.getElementById("Fase")
+        for(i = 0; i < elements.length; i++){
+            var doc = document.createElement("div");
+            doc.className = "col-sm"
+            DOM_div.appendChild(doc)
+            var Image = document.createElement("img");
+            Image.className = "enig-img"
+            Image.src = elements[i].src
+            doc.appendChild(Image)
+        }
+    }
+
+    /* Function that randoms the array of elements */
+    function randomSort(elements) {
+        var randomElements = [];
+        while (randomElements.length !== elements.length) {
+            var i = Math.floor(Math.random() * elements.length);
+            if (randomElements.indexOf(elements[i]) < 0) {
+                randomElements.push(elements[i]);
+            }
+        }
+        return randomElements;
+    }
+}
+
+/* function verify(){
+    var answer = getDocumentById("answer")
+    var number = getDocumentById("faseNum")
+    if(rightAnswers(number, answer) == true){
+        console.log("Certo")
+    } else {
+        console.log("errado")
+    }
+}
+function rightAnswers(){
+    answer.toLowerCase()
+    if(number == 0){
+        if( answer == "sódio" || answer == "sodio"){
+            return true
+        }
+        return false
+    }
+    
+    if(number == 1){
+        if( answer == "kriptônio" || answer == "kriptonio"){
+            return true
+        }
+        return false
+    }
+    if(number == 2){
+        if( answer == "fósforo" || answer == "fosforo"){
+            return true
+        }
+        return false
+    }
+    if(number == 3){
+        if( answer == "einstênio" || answer == "einstenio"){
+            return true
+        }
+        return false
+    }
+    if(number == 4){
+        if( answer == "mendeleev"){
+            return true
+        }
+        return false
+    } */
+/*}*/
