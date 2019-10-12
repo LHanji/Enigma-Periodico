c7157d25994fb64ae1c9be66099cd6a18dc89873
Shaiene-kun <48965886+Shaiene-kun@users.noreply.github.com>
2019-10-12 13:22:09 -0300

    Reorganização
    Eu fiz essa reoganizarção pra que cada um coloque as paradas nas suas pastas.

---
+++ Tralhas/Fase 1/script.js
@@ -0,0 +1,7 @@
+function pass() {
+    if (document.getElementById("password").value == "00110010 00111000 00110110") {
+        location.href = "https://www.youtube.com/watch?v=1Mcdh2Vf2Xk";
+    } else{
+        alert('Você errou :c');
+    }
+}
