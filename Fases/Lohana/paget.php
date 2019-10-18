840cc8981ffc6e5c7067f50ddbf8ac218bd5f70f
Yasmin L <48965772+yasmin-liban@users.noreply.github.com>
2019-10-18 10:46:56 -0300

    Add files via upload

--- Fases/Lohana/paget.php
+++ Fases/Lohana/paget.php
@@ -10,5 +10,6 @@
         echo '<script src="script.js"></script>';
     }
     $elemento = '"a"';
+    $tabela = "'../Arquivos Compartilhados/tabela1.jpg'";
 
     include  '../Arquivos Compartilhados/template.php';
