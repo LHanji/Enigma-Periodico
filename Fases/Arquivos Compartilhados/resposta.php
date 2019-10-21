1c5e3eb6149f97a5a000e86a9df6f84278c0808b
Shaiene-kun <48965886+Shaiene-kun@users.noreply.github.com>
2019-10-21 20:32:02 -0300

    Acho que foi finalemnte hein galera

--- Fases/Arquivos Compartilhados/resposta.php
+++ Fases/Arquivos Compartilhados/resposta.php
@@ -8,7 +8,7 @@ $array_enig = $_SESSION['array_enig']; //Essa var possui quais os desafios foram
 
 //FIM DO JOGO
 if (($array_enig["Yasmin"] == true) and ($array_enig["Shaiene"] == true) and ($array_enig["Arlene"] == true) and ($array_enig["Lohana"] == true) and ($array_enig["Gean"] == true)) {
-    header('Location: endPage.html');
+    header('Location: end.php');
 }
 
 
@@ -19,7 +19,7 @@ switch ($ult_enig) {
         if ($resp == "carbono") {
             $array_enig["Yasmin"] = true;
             $_SESSION['array_enig'] = $array_enig;
-            nextChallenge();
+            nextChallenge($array_enig);
         } else {
             header('Location: ../Yasmin/faseYasmin.php');
         }
@@ -28,7 +28,7 @@ switch ($ult_enig) {
     case "Shaiene":
         $array_enig["Shaiene"] = true;
         $_SESSION['array_enig'] = $array_enig;
-        nextChallenge();
+        nextChallenge($array_enig);
         break;
 
         //Insiram aqui, a verificação de vocês, lembrem-se de alternarem a vossa chave para true
@@ -37,7 +37,7 @@ switch ($ult_enig) {
         if ($resp == "cloro") {
             $array_enig["Arlene"] = true;
             $_SESSION['array_enig'] = $array_enig;
-            nextChallenge();
+            nextChallenge($array_enig);
         } else {
             header('Location: ../Arlene/principal1.php');
         }
@@ -45,7 +45,7 @@ switch ($ult_enig) {
     case "Lohana":
         $array_enig["Lohana"] = true;
         $_SESSION['array_enig'] = $array_enig;
-        nextChallenge();
+        nextChallenge($array_enig);
         break;
 
     case "Gean":
@@ -53,61 +53,74 @@ switch ($ult_enig) {
       if (($resp == "tecnecio")||($resp == "tecnécio")||($resp == "tc")){
           $array_enig["Gean"] = true;
           $_SESSION['array_enig'] = $array_enig;
-          nextChallenge();
+          nextChallenge($array_enig);
       } else {
           header('Location: ../Gean/index.php');
       }
       break;
 }
 
-function nextChallenge()
+function nextChallenge($array_enig)
 { //Essa função recursiva seleciona a próxima fase, mudem no seu trecho o nome do seu arquivo
-    $rand = mt_rand(1, 5);
-    switch ($rand) {
-        case 1:
-            if ($array_enig["Yasmin"] == false)
-                header('Location: ../Yasmin/faseYasmin.php');
-            else
-                end();
-            nextChallenge();
-            break;
-
-        case 2:
-            if ($array_enig["Shaiene"] == false)
-                header('Location: ../Shaiene/RS.php');
-            else
-                end();
-            nextChallenge();
-            break;
-
-        case 3:
-            if ($array_enig["Arlene"] == false)
-                header('Location: ../Arlene/principal1.php');
-            else
-                end();
-            nextChallenge();
-            break;
-
-        case 4:
-            if ($array_enig["Lohana"] == false)
-                header('Location: ../Lohana/paget.php');
-            else
-                end();
-            nextChallenge();
-            break;
-
-        case 5:
-            if ($array_enig["Gean"] == false)
-                header('Location: ../Gean/index.php');
-            else
-                end();
-            nextChallenge();
-            break;
+    if (($array_enig["Yasmin"] == true) and ($array_enig["Shaiene"] == true) and ($array_enig["Arlene"] == true) and ($array_enig["Lohana"] == true) and ($array_enig["Gean"] == true)) {
+        header('Location: end.php');
+        exit();
     }
-    function end()
-    {
-        if ($array_enig["Gean"] == true && $array_enig["Lohana"] == true && $array_enig["Arlene"] == true && $array_enig["Shaiene"] == true && $array_enig["Yasmin"] == true) {
-            header('Location: end.php');
-        }
+    $aux = false;
+    while($aux == false){
+        $rand = mt_rand(1, 5);
+        switch ($rand) {
+            case 1:
+                if ($array_enig["Yasmin"] == false){
+                    header('Location: ../Yasmin/faseYasmin.php');
+                    exit();
+                }
+                    
+                //else
+                  //  end($array_enig);
+                break;
+    
+            case 2:
+                if ($array_enig["Shaiene"] == false){
+                    header('Location: ../Shaiene/RS.php');
+                    exit();    
+                }
+                //else
+                    //end($array_enig);
+                break;
+    
+            case 3:
+                if ($array_enig["Arlene"] == false){
+                    header('Location: ../Arlene/principal1.php');
+                    exit();
+                }
+                //else
+                  //  end($array_enig);
+                
+                break;
+    
+            case 4:
+                if ($array_enig["Lohana"] == false)
+                    {header('Location: ../Lohana/paget.php');
+                    exit();
+                }
+                //else
+                  //  end($array_enig);
+                break;
+    
+            case 5:
+                if ($array_enig["Gean"] == false)
+                    {header('Location: ../Gean/index.php');
+                    exir();
+                    }
+                //else
+                  //  end($array_enig);
+                
+                break;
+            }
+    
+    
     }
+    header('Location: end.php');
+   
 }
